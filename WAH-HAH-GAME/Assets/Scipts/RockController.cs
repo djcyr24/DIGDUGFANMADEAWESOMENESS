132d9f840cdd8e1bdac25572a59c181965785146
DESKTOP-LV3OVPQ\Jim <djcyr24@hotmail.com>
2018-12-11 14:06:04 -0400

    First Commit of WahHah Game have alot of art and animation done

---
+++ WAH-HAH-GAME/Assets/Scipts/RockController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RockController : MonoBehaviour {
+
+	void Start ()
+    {
+        GetComponent<PolygonCollider2D>().enabled = false;
+	}
+	
+	void Update ()
+    {
+        GetComponent<PolygonCollider2D>().enabled = true;
+
+        if (GameFlow.supportGone == "y")
+        {
+            StartCoroutine(dropRock());
+            GameFlow.supportGone = "n";
+        }
+		
+	}
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        Destroy(gameObject);
+    }
+
+    IEnumerator dropRock()
+    {
+        yield return new WaitForSeconds(3);
+        GetComponent<Rigidbody2D>().gravityScale = 1;
+    }
+}
